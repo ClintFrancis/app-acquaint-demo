64ea680346f1d579f20fa9de50ae4fae254976f9
Clinton Francis <clfranci@microsoft.com>
2017-07-10 12:39:07 -0400

    Updated package references

---
+++ IOS_XTC.sh
@@ -0,0 +1,2 @@
+echo mobile-center test run uitest --app \"clfranci-ooow/acquaint-production\" --devices dd1a9d4a --app-path Acquaint.Native/Acquaint.Native.iOS/bin/iPhone/Debug/AcquaintNativeiOS.ipa  --test-series \"master\" --locale \"en_US\" --build-dir Acquaint.Native/Acquaint.Native.UITest/Acquaint.Native.UITest.iOS/bin/Debug
+ 
